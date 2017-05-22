1e024f067b5f0305ed94ff09cab6afbdd1a50946
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-05-22 15:26:38 +0200

    [MIG] Clean the code to respect PEP8 (remove useless imports, remove useless spaces, ...) and fix some small bugs
    (cherry picked from commit efd9e9f8efe9a5e8da791d179b5a5d57710a31c3)
    
    [FIX] Add space to respect PEP8
    
    (cherry picked from commit 06c93c610646bb4269f303c3a7a55700cc6dfd1f)
    
    [FIX] Replace the field "full_reconcile_id" by "reconciled" in a search domain on account.move.line
    
    (cherry picked from commit cf2cfd43e34dff9eef2571b87de777a183c220c0)

--- account_credit_control/models/credit_control_line.py
+++ account_credit_control/models/credit_control_line.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Nicolas Bessi, Guewen Baconnier
-#    Copyright 2012-2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012-2017 Camptocamp SA
+# Copyright 2017 Okia SPRL (https://okia.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
 
-from odoo import models, fields, api, _
-from odoo.exceptions import Warning
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 logger = logging.getLogger(__name__)
 
@@ -226,7 +210,7 @@ class CreditControlLine(models.Model):
     def unlink(self):
         for line in self:
             if line.state != 'draft':
-                raise Warning(
+                raise UserError(
                     _('You are not allowed to delete a credit control '
                       'line that is not in draft state.')
                 )
