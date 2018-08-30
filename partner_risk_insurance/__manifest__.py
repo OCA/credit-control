05b5b3372208d40438b38fe0f9043ff473c6b696
sergio.teruel <sergio.teruel@tecnativa.com>
2018-08-30 12:20:38 +0200

    [11.0][MIG] partner_risk_insurance: Migration to v11.0

---
+++ partner_risk_insurance/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Risk Insurance",
+    "version": "11.0.1.0.0",
+    "development_status": "Production/Stable",
+    "summary": "Risk insurance partner information",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Tecnativa - Pedro M. Baeza,"
+              "Factor Libre S.L,"
+              "NaN·tic,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "website": "https://github.com/OCA/credit-control",
+    "depends": [
+        'base',
+    ],
+    "category": "Credit Control",
+    "data": [
+        'views/res_partner_view.xml',
+    ],
+    "installable": True
+}
