156d47c37ccff1229e5f3f38a06778b6015c5513
ernesto <ernesto.tejeda87@gmail.com>
2019-02-26 08:30:53 -0500

    [MIG] stock_financial_risk: Migration to 12.0 and renaming
    [OCA/partner-contact] partner_stock_risk > stock_financial_risk
    and Migration to 12.0

--- stock_financial_risk/__manifest__.py
+++ stock_financial_risk/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Partner Stock Risk',
     'summary': 'Manage partner risk in stock moves',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
-    'author': 'Tecnativa,'
+    'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'https://www.tecnativa.com',
+    'website': 'https://github.com/OCA/credit-control',
     'depends': [
         'stock',
-        'partner_financial_risk'
+        'account_financial_risk'
     ],
     'installable': True,
 }
---
+++ stock_financial_risk/readme/USAGE.rst
@@ -0,0 +1,6 @@
+To use this module, you need to:
+
+#. Go to *Customers > Financial Risk*
+#. Set limits and choose options to compute in credit limit
+#. Go to *Inventory > Operations > Transfers*
+#. Try transfer a risk exceed partner picking
