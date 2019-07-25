eec3ee358fe4b5f2b311b34926f40be6398e3644
Quentin Groulard <quentin.groulard@acsone.eu>
2019-07-25 15:55:57 +0200

    [FIX] Travis errors

--- account_credit_control_analysis/report/account_credit_control_analysis.py
+++ account_credit_control_analysis/report/account_credit_control_analysis.py
@@ -23,8 +23,8 @@ class AccountCreditControlAnalysis(models.Model):
         comodel_name="res.currency", string="Currency", readonly=True
     )
     policy_level_id = fields.Many2one(
-        comodel_name="credit.control.policy.level", 
-        string="Overdue Level", 
+        comodel_name="credit.control.policy.level",
+        string="Overdue Level",
         readonly=True,
     )
     level = fields.Integer(string="Level", readonly=True)
@@ -45,20 +45,28 @@ class AccountCreditControlAnalysis(models.Model):
                    ccl.currency_id                          AS currency_id,
                    ccl.policy_level_id                      AS policy_level_id,
                    ccpl.level                               AS level,
-                (SELECT CASE 
-                    WHEN ccl.currency_id IS NOT NULL THEN sum(amount_residual_currency)
+                (SELECT CASE
+                    WHEN ccl.currency_id IS NOT NULL
+                    THEN sum(amount_residual_currency)
                     ELSE sum(amount_residual)
                     END
                 FROM account_move_line AS aml
                 WHERE NOT aml.reconciled
-                AND aml.id IN 
+                AND aml.id IN
                     (SELECT move_line_id
-                    FROM credit_control_line AS ccl2 
+                    FROM credit_control_line AS ccl2
                     WHERE ccl2.partner_id=ccl.partner_id
                         AND ccl2.policy_id=ccl.policy_id
-                        AND ((ccl.currency_id IS NULL AND ccl2.currency_id IS NULL) OR ccl2.currency_id=ccl.currency_id))) AS open_balance
+                        AND (
+                            (ccl.currency_id IS NULL
+                            AND ccl2.currency_id IS NULL)
+                        OR ccl2.currency_id=ccl.currency_id
+                        )
+                    )
+                ) AS open_balance
             FROM credit_control_line AS ccl
-            LEFT JOIN credit_control_policy_level AS ccpl ON ccpl.id=ccl.policy_level_id
+            LEFT JOIN credit_control_policy_level AS ccpl
+            ON ccpl.id=ccl.policy_level_id
             ORDER BY ccl.partner_id,
                     ccl.policy_id,
                     ccl.currency_id,
