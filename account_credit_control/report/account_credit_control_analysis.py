6fc7934cdc8aaf1c0097de2a5577c8855c66942b
Quentin Groulard <quentin.groulard@acsone.eu>
2019-10-14 10:27:40 +0200

    [FIX+IMP+REF] account_credit_control: General refactoring
    * [IMP] Improve create_or_update_from_mv_lines performances
    * [IMP] Pass manual_followup in credit_control_line create vals
    * [REF] Move Credit Control related fields on res.partner to a dedicated page
    * [IMP] Credit Control Analysis
    * Add credit control notes (free text) on lines, move_lines, invoices and partners
    * Add a credit control date on invoices and move_lines to ignore Credit Control runs until this date
    * Add 'phone' channel
    * Therefore rename to_be_send -> to_do, sent -> done and date_sent -> date_reminded
    * track_visibility="on_change" on credit control line state
    * Display level_lines max level when grouped (groupby in tree view) instead of sum
    * Add a group by move_line_journals on credit control lines
    * Fallback to letter if channel is 'email' and partner has no email
    * Add config on policy to set the policy has default on partners
    * Dates format according to current user settings
    * Fix error message when trying to create a run at a previous date than last run
    * Add specific email for credit control on partner
    * [IMP] Extensibility + performances
      Performances by joining res_partner tab
    * [FIX] Use sudo to set default policy on partner
      Otherwise users trying to create partners get an access right error if they don't have read access on credit control policy
    * [FIX] Remove followup group's string
    * [IMP] Send emails with jobs
    * [REF] Clean features
      This MR is based on a v10 MR which contains many features we don't use and test.
      This commit removes thoses in order to lighten this MR.
    *  Use default date format on runs
    * [REF] Refactor generate credit lines
    * [FIX] Policy generate lines based on controlling date (not whole run record)
    * [FIX]  move do_nothing check
    * [IMP] Add Partner Payment Action Type

--- account_credit_control/report/account_credit_control_analysis.py
+++ account_credit_control/report/account_credit_control_analysis.py
@@ -13,9 +13,7 @@ class AccountCreditControlAnalysis(models.Model):
     partner_id = fields.Many2one(
         comodel_name="res.partner", readonly=True
     )
-    partner_ref = fields.Char(
-        related="partner_id.ref", string="Partner Ref", readonly=True
-    )
+    partner_ref = fields.Char(string="Partner Ref", readonly=True)
     policy_id = fields.Many2one(
         comodel_name="credit.control.policy", string="Policy", readonly=True
     )
@@ -27,35 +25,43 @@ class AccountCreditControlAnalysis(models.Model):
         string="Overdue Level",
         readonly=True,
     )
-    level = fields.Integer(string="Level", readonly=True)
+    level = fields.Integer(string="Max Level", readonly=True)
     open_balance = fields.Float(
+        string="Overdue Balance",
         readonly=True,
         help="Open balance on credit control lines"
         "of same partner, policy and currency",
     )
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        readonly=True,
+    )
 
-    @api.model_cr
-    def init(self):
-        tools.drop_view_if_exists(self._cr, "credit_control_analysis")
-        self._cr.execute(
+    def _distinct_fields(self):
+        return (
             """
-            CREATE VIEW credit_control_analysis
-            AS
-            (SELECT DISTINCT ON (ccl.commercial_partner_id,
-                   ccl.policy_id,
-                   ccl.currency_id) ccl.id                  AS id,
-                   ccl.commercial_partner_id                AS partner_id,
-                   ccl.policy_id                            AS policy_id,
-                   ccl.currency_id                          AS currency_id,
-                   ccl.policy_level_id                      AS policy_level_id,
-                   ccpl.level                               AS level,
-                (SELECT sum(amount_residual)
+            partner.id, ccl.policy_id, ccl.currency_id
+            """
+        )
+
+    def _fields_to_select(self):
+        return (
+            """
+            ccl.id                                   AS id,
+            partner.id                               AS partner_id,
+            partner.ref                              AS partner_ref,
+            ccl.policy_id                            AS policy_id,
+            ccl.currency_id                          AS currency_id,
+            ccl.policy_level_id                      AS policy_level_id,
+            ccl.company_id                           AS company_id,
+            ccpl.level                               AS level,
+            (SELECT sum(amount_residual)
                 FROM account_move_line AS aml
                 WHERE NOT aml.reconciled
                 AND aml.id IN
                     (SELECT move_line_id
                     FROM credit_control_line AS ccl2
-                    WHERE ccl2.commercial_partner_id=ccl.commercial_partner_id
+                    WHERE ccl2.commercial_partner_id=partner.id
                         AND ccl2.policy_id=ccl.policy_id
                         AND (
                             (ccl.currency_id IS NULL
@@ -64,15 +70,47 @@ class AccountCreditControlAnalysis(models.Model):
                         )
                     )
                 ) AS open_balance
+            """
+        )
+
+    def _from_tables(self):
+        return (
+            """
             FROM credit_control_line AS ccl
             LEFT JOIN credit_control_policy_level AS ccpl
             ON ccpl.id=ccl.policy_level_id
             INNER JOIN account_move_line AS aml
             ON aml.id=ccl.move_line_id AND NOT aml.reconciled
-            ORDER BY ccl.commercial_partner_id,
-                    ccl.policy_id,
-                    ccl.currency_id,
-                    ccpl.level DESC,
-                    ccl.id)
+            LEFT JOIN res_partner AS partner
+            ON partner.id=ccl.commercial_partner_id
             """
         )
+
+    def _order_by(self):
+        return (
+            """
+            partner.id, ccl.policy_id, ccl.currency_id, ccpl.level DESC, ccl.id
+            """
+        )
+
+    def _get_sql_query(self):
+        return (
+            """
+            CREATE VIEW credit_control_analysis
+            AS
+            (SELECT DISTINCT ON (%s) %s
+            %s
+            ORDER BY %s)
+            """ % (
+                self._distinct_fields(),
+                self._fields_to_select(),
+                self._from_tables(),
+                self._order_by(),
+            )
+        )
+
+    @api.model_cr
+    def init(self):
+        tools.drop_view_if_exists(self._cr, "credit_control_analysis")
+        query = self._get_sql_query()
+        self._cr.execute(query)
