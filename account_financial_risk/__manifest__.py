7956c76333504c1a6503d83862702a4e64a787fb
JesusVMayor <jesus@comunitea.com>
2019-11-14 16:46:39 +0100

    [FIX]account_financial_risk: risk_exception field explicitly setted to False.

--- account_financial_risk/__manifest__.py
+++ account_financial_risk/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Account Financial Risk',
     'summary': 'Manage customer risk',
-    'version': '12.0.1.1.0',
+    'version': '12.0.1.1.1',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'author': 'Tecnativa, Odoo Community Association (OCA)',
