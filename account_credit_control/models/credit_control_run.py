c0607a967adc63e70d6cd9d78bdd63479cb7cd6e
Quentin Groulard <quentin.groulard@acsone.eu>
2019-11-19 09:20:55 +0100

    [FIX] Policy generate lines based on controlling date (not whole run record)

--- account_credit_control/models/credit_control_run.py
+++ account_credit_control/models/credit_control_run.py
@@ -131,7 +131,7 @@ class CreditControlRun(models.Model):
                 policy_manual_lines,
                 policy_lines_generated,
                 policy_report,
-            ) = policy.generate_credit_lines(self)
+            ) = policy.generate_credit_lines(self.date)
             manually_managed_lines |= policy_manual_lines
             generated |= policy_lines_generated
             report += policy_report
