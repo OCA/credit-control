c4056e4c69a7a2dff1d7cd5cd6814bc3d34f73b4
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-15 08:55:32 -0600

    [IMP] stock_financial_risk: black, isort

--- stock_financial_risk/__manifest__.py
+++ stock_financial_risk/__manifest__.py
@@ -2,17 +2,13 @@
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Partner Stock Risk',
-    'summary': 'Manage partner risk in stock moves',
-    'version': '12.0.1.0.0',
-    'category': 'Sales Management',
-    'license': 'AGPL-3',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/credit-control',
-    'depends': [
-        'stock',
-        'account_financial_risk'
-    ],
-    'installable': True,
+    "name": "Partner Stock Risk",
+    "summary": "Manage partner risk in stock moves",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/credit-control",
+    "depends": ["stock", "account_financial_risk"],
+    "installable": True,
 }
