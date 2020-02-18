8808645eebb53a085da72b804681f870f4dc3edb
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-18 11:58:15 +0100

    [IMP] account_credit_control: black, isort, prettier

--- account_credit_control/wizard/credit_control_emailer.py
+++ account_credit_control/wizard/credit_control_emailer.py
@@ -11,44 +11,42 @@ class CreditControlEmailer(models.TransientModel):
 
     _name = "credit.control.emailer"
     _description = """Mass credit line emailer"""
-    _rec_name = 'id'
+    _rec_name = "id"
 
     @api.model
     def _get_line_ids(self):
         context = self.env.context
-        if context.get('active_model') != 'credit.control.line' or \
-                not context.get('active_ids'):
+        if context.get("active_model") != "credit.control.line" or not context.get(
+            "active_ids"
+        ):
             return False
-        line_obj = self.env['credit.control.line']
-        lines = line_obj.browse(context['active_ids'])
+        line_obj = self.env["credit.control.line"]
+        lines = line_obj.browse(context["active_ids"])
         return self._filter_lines(lines)
 
     line_ids = fields.Many2many(
-        comodel_name='credit.control.line',
-        string='Credit Control Lines',
+        comodel_name="credit.control.line",
+        string="Credit Control Lines",
         default=lambda self: self._get_line_ids(),
-        domain=[
-            ('state', '=', 'to_be_sent'),
-            ('channel', '=', 'email'),
-        ],
+        domain=[("state", "=", "to_be_sent"), ("channel", "=", "email")],
     )
 
     @api.model
-    @api.returns('credit.control.line')
+    @api.returns("credit.control.line")
     def _filter_lines(self, lines):
         """ filter lines to use in the wizard """
-        line_obj = self.env['credit.control.line']
+        line_obj = self.env["credit.control.line"]
         domain = [
-            ('state', '=', 'to_be_sent'),
-            ('id', 'in', lines.ids),
-            ('channel', '=', 'email'),
+            ("state", "=", "to_be_sent"),
+            ("id", "in", lines.ids),
+            ("channel", "=", "email"),
         ]
         return line_obj.search(domain)
 
     @api.multi
     def _send_emails(self):
         self.ensure_one()
-        comm_obj = self.env['credit.control.communication']
+        comm_obj = self.env["credit.control.communication"]
 
         filtered_lines = self._filter_lines(self.line_ids)
         comms = comm_obj._generate_comm_from_credit_lines(filtered_lines)
@@ -58,7 +56,7 @@ class CreditControlEmailer(models.TransientModel):
     def email_lines(self):
         self.ensure_one()
         if not self.line_ids:
-            raise UserError(_('No credit control lines selected.'))
+            raise UserError(_("No credit control lines selected."))
 
         self._send_emails()
-        return {'type': 'ir.actions.act_window_close'}
+        return {"type": "ir.actions.act_window_close"}
