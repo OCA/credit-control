52060af4b904601f7b4d7a4a9faebe648102bdce
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-18 11:58:15 +0100

    [IMP] account_credit_control: black, isort, prettier

--- account_credit_control/wizard/credit_control_communication.py
+++ account_credit_control/wizard/credit_control_communication.py
@@ -11,90 +11,73 @@ class CreditControlCommunication(models.TransientModel):
     will exist even if not saved
 
     """
+
     _name = "credit.control.communication"
     _description = "credit control communication"
-    _rec_name = 'partner_id'
+    _rec_name = "partner_id"
 
     partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Partner',
-        required=True,
+        comodel_name="res.partner", string="Partner", required=True
     )
     current_policy_level = fields.Many2one(
-        comodel_name='credit.control.policy.level',
-        string='Level',
-        required=True,
+        comodel_name="credit.control.policy.level", string="Level", required=True
     )
     currency_id = fields.Many2one(
-        comodel_name='res.currency',
-        string='Currency',
-        required=True,
+        comodel_name="res.currency", string="Currency", required=True
     )
     credit_control_line_ids = fields.Many2many(
-        comodel_name='credit.control.line',
-        rel='comm_credit_rel',
-        string='Credit Lines',
+        comodel_name="credit.control.line", rel="comm_credit_rel", string="Credit Lines"
     )
-    contact_address = fields.Many2one(
-        comodel_name='res.partner',
-        readonly=True,
-    )
-    report_date = fields.Date(
-        default=lambda self: fields.Date.context_today(self),
-    )
+    contact_address = fields.Many2one(comodel_name="res.partner", readonly=True)
+    report_date = fields.Date(default=lambda self: fields.Date.context_today(self))
 
     @api.model
     def _get_company(self):
-        company_obj = self.env['res.company']
-        return company_obj._company_default_get('credit.control.policy')
+        company_obj = self.env["res.company"]
+        return company_obj._company_default_get("credit.control.policy")
 
     company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Company',
+        comodel_name="res.company",
+        string="Company",
         default=lambda self: self._get_company(),
         required=True,
     )
     user_id = fields.Many2one(
-        comodel_name='res.users',
-        default=lambda self: self.env.user,
-        string='User',
+        comodel_name="res.users", default=lambda self: self.env.user, string="User"
     )
-    total_invoiced = fields.Float(
-        compute='_compute_total',
-    )
-    total_due = fields.Float(
-        compute='_compute_total',
-    )
+    total_invoiced = fields.Float(compute="_compute_total")
+    total_due = fields.Float(compute="_compute_total")
 
     @api.model
     def _get_total(self):
-        amount_field = 'credit_control_line_ids.amount_due'
+        amount_field = "credit_control_line_ids.amount_due"
         return sum(self.mapped(amount_field))
 
     @api.model
     def _get_total_due(self):
-        balance_field = 'credit_control_line_ids.balance_due'
+        balance_field = "credit_control_line_ids.balance_due"
         return sum(self.mapped(balance_field))
 
     @api.multi
-    @api.depends('credit_control_line_ids',
-                 'credit_control_line_ids.amount_due',
-                 'credit_control_line_ids.balance_due')
+    @api.depends(
+        "credit_control_line_ids",
+        "credit_control_line_ids.amount_due",
+        "credit_control_line_ids.balance_due",
+    )
     def _compute_total(self):
         for communication in self:
             communication.total_invoiced = communication._get_total()
             communication.total_due = communication._get_total_due()
 
     @api.model_create_multi
-    @api.returns('self', lambda value: value.id)
+    @api.returns("self", lambda value: value.id)
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get('partner_id'):
+            if vals.get("partner_id"):
                 # the computed field does not work in TransientModel,
                 # just set a value on creation
-                partner_id = vals['partner_id']
-                vals['contact_address'] = \
-                    self._get_contact_address(partner_id).id
+                partner_id = vals["partner_id"]
+                vals["contact_address"] = self._get_contact_address(partner_id).id
         return super(CreditControlCommunication, self).create(vals_list)
 
     @api.multi
@@ -108,32 +91,34 @@ class CreditControlCommunication(models.TransientModel):
         return email
 
     @api.multi
-    @api.returns('res.partner')
+    @api.returns("res.partner")
     def get_contact_address(self):
         """ Compatibility method, please use the contact_address field """
         self.ensure_one()
         return self.contact_address
 
     @api.model
-    @api.returns('res.partner')
+    @api.returns("res.partner")
     def _get_contact_address(self, partner_id):
-        partner_obj = self.env['res.partner']
+        partner_obj = self.env["res.partner"]
         partner = partner_obj.browse(partner_id)
-        add_ids = partner.address_get(adr_pref=['invoice']) or {}
-        add_id = add_ids['invoice']
+        add_ids = partner.address_get(adr_pref=["invoice"]) or {}
+        add_id = add_ids["invoice"]
         return partner_obj.browse(add_id)
 
     @api.model
-    @api.returns('credit.control.line')
+    @api.returns("credit.control.line")
     def _get_credit_lines(self, line_ids, partner_id, level_id, currency_id):
         """ Return credit lines related to a partner and a policy level """
-        cr_line_obj = self.env['credit.control.line']
-        cr_lines = cr_line_obj.search([
-            ('id', 'in', line_ids),
-            ('partner_id', '=', partner_id),
-            ('policy_level_id', '=', level_id),
-            ('currency_id', '=', currency_id),
-        ])
+        cr_line_obj = self.env["credit.control.line"]
+        cr_lines = cr_line_obj.search(
+            [
+                ("id", "in", line_ids),
+                ("partner_id", "=", partner_id),
+                ("policy_level_id", "=", level_id),
+                ("currency_id", "=", currency_id),
+            ]
+        )
         return cr_lines
 
     @api.model
@@ -155,7 +140,7 @@ class CreditControlCommunication(models.TransientModel):
             "          credit_control_line.currency_id"
         )
         cr = self.env.cr
-        cr.execute(sql, (tuple(lines.ids), ))
+        cr.execute(sql, (tuple(lines.ids),))
         res = cr.dictfetchall()
         company_currency = self.env.user.company_id.currency_id
         datas = []
@@ -163,14 +148,14 @@ class CreditControlCommunication(models.TransientModel):
             data = {}
             level_lines = self._get_credit_lines(
                 lines.ids,
-                group['partner_id'],
-                group['policy_level_id'],
-                group['currency_id'],
+                group["partner_id"],
+                group["policy_level_id"],
+                group["currency_id"],
             )
-            data['credit_control_line_ids'] = [(6, 0, level_lines.ids)]
-            data['partner_id'] = group['partner_id']
-            data['current_policy_level'] = group['policy_level_id']
-            data['currency_id'] = group['currency_id'] or company_currency.id
+            data["credit_control_line_ids"] = [(6, 0, level_lines.ids)]
+            data["partner_id"] = group["partner_id"]
+            data["current_policy_level"] = group["policy_level_id"]
+            data["currency_id"] = group["currency_id"] or company_currency.id
             datas.append(data)
         return datas
 
@@ -183,57 +168,58 @@ class CreditControlCommunication(models.TransientModel):
         return comms
 
     @api.multi
-    @api.returns('mail.mail')
+    @api.returns("mail.mail")
     def _generate_emails(self):
         """ Generate email message using template related to level """
-        emails = self.env['mail.mail']
-        required_fields = [
-            'subject',
-            'body_html',
-            'email_from',
-            'email_to',
-        ]
+        emails = self.env["mail.mail"]
+        required_fields = ["subject", "body_html", "email_from", "email_to"]
         for comm in self:
             template = comm.current_policy_level.email_template_id
             email_values = template.generate_email(comm.id)
-            email_values['message_type'] = 'email'
+            email_values["message_type"] = "email"
             # model is Transient record (self) removed periodically so no point
             # of storing res_id
-            email_values.pop('model', None)
-            email_values.pop('res_id', None)
+            email_values.pop("model", None)
+            email_values.pop("res_id", None)
             # Remove when mail.template returns correct format attachments
-            attachment_list = email_values.pop('attachments', [])
+            attachment_list = email_values.pop("attachments", [])
             email = emails.create(email_values)
 
-            state = 'sent'
+            state = "sent"
             # The mail will not be send, however it will be in the pool, in an
             # error state. So we create it, link it with
             # the credit control line
             # and put this latter in a `email_error` state we not that we have
             # a problem with the email
             if not all(email_values.get(field) for field in required_fields):
-                state = 'email_error'
-            comm.credit_control_line_ids.write({
-                'mail_message_id': email.id,
-                'state': state,
-            })
-            email.attachment_ids = [(0, 0, {
-                'name': att[0],
-                'datas': att[1],
-                'datas_fname': att[0],
-                'res_model': 'mail.mail',
-                'res_id': email.id,
-                'type': 'binary',
-            }) for att in attachment_list]
+                state = "email_error"
+            comm.credit_control_line_ids.write(
+                {"mail_message_id": email.id, "state": state}
+            )
+            email.attachment_ids = [
+                (
+                    0,
+                    0,
+                    {
+                        "name": att[0],
+                        "datas": att[1],
+                        "datas_fname": att[0],
+                        "res_model": "mail.mail",
+                        "res_id": email.id,
+                        "type": "binary",
+                    },
+                )
+                for att in attachment_list
+            ]
             emails |= email
         return emails
 
     @api.multi
-    @api.returns('credit.control.line')
+    @api.returns("credit.control.line")
     def _mark_credit_line_as_sent(self):
-        lines = self.env['credit.control.line']
+        lines = self.env["credit.control.line"]
         for comm in self:
             lines |= comm.credit_control_line_ids
 
-        lines.write({'state': 'sent'})
+        lines.write({"state": "sent"})
         return lines
