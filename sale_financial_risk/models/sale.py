0f1ad8016827386d938e39cf2dec332d02f3a7c0
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-05-26 04:41:36 +0200

    [IMP] account_financial_risk account_payment_return_financial_risk sale_financial_risk:  - Optimize code and change stored computed fields to not store to improve multi-company support  - Improve multi-currency support  - Convert risk amount fields to clickable link that shows traceability of amount origin  - New pivot views to risk amount traceability  - Simplify class style applied on risk fields  - Migration script to remove old stored computed fields  - Improve tests to cover new functionallity  - Update translation files
    account_financial_risk:
     - Don't block refund invoice validation when partner has risk exception
     - Allow search partners by risk exception field
     - Remove obsolete cron
    
    sale_financial_risk
     - Create related store commercial_partner_id field in sale order line to simplify computation
     - Rename amt_to_invoice field to risk_amount in sale order line
     - Hook and migration scripts to reduce new fields computing time
    
    TT23765

--- sale_financial_risk/models/sale.py
+++ sale_financial_risk/models/sale.py
@@ -1,7 +1,8 @@
-# Copyright 2016-2018 Tecnativa - Carlos Dauden
+# Copyright 2016-2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
+from odoo.tools import float_round
 
 
 class SaleOrder(models.Model):
@@ -10,17 +11,20 @@ class SaleOrder(models.Model):
     @api.multi
     def action_confirm(self):
         if not self.env.context.get('bypass_risk', False):
+            risk_amount = self.currency_id._convert(
+                self.amount_total, self.company_id.currency_id,
+                self.company_id, fields.Date.today())
             partner = self.partner_id.commercial_partner_id
             exception_msg = ""
             if partner.risk_exception:
                 exception_msg = _("Financial risk exceeded.\n")
             elif partner.risk_sale_order_limit and (
-                    (partner.risk_sale_order + self.amount_total) >
+                    (partner.risk_sale_order + risk_amount) >
                     partner.risk_sale_order_limit):
                 exception_msg = _(
                     "This sale order exceeds the sales orders risk.\n")
             elif partner.risk_sale_order_include and (
-                    (partner.risk_total + self.amount_total) >
+                    (partner.risk_total + risk_amount) >
                     partner.credit_limit):
                 exception_msg = _(
                     "This sale order exceeds the financial risk.\n")
@@ -37,54 +41,52 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    amt_to_invoice = fields.Monetary(
-        string="Amount To Invoice",
-        compute='_compute_amount_to_invoice',
+    risk_amount = fields.Monetary(
+        string="Risk amount",
+        compute='_compute_risk_amount',
         compute_sudo=True,
         store=True
+    )
+    # TODO: Analyze performance vs order_id.partner_id.commercial_partner_id
+    commercial_partner_id = fields.Many2one(
+        related='order_partner_id.commercial_partner_id',
+        string='Commercial Entity',
+        store=True,
+        index=True,
     )
 
     @api.depends('state',
                  'price_reduce_taxinc',
                  'qty_delivered',
-                 'invoice_lines',
-                 'invoice_lines.price_total',
-                 'invoice_lines.invoice_id',
-                 'invoice_lines.invoice_id.state',
-                 'invoice_lines.invoice_id.type')
-    def _compute_amount_to_invoice(self):
-        """ Compute the taxed amount already invoiced from the sale order
-            line. This amount is computed as
-                SUM(inv_line.price_total) - SUM(ref_line.price_total)
-            where
-                `inv_line` is a customer invoice line linked to the SO line
-                `ref_line` is a customer credit note (refund) line linked to
-                    the SO line
-
-            Then, compute amount to invoice as:
-                total_sale_line - amount_invoiced
-            where
-                `amount_invoiced` is taxed amount previously explained
-        """
-        for line in self.filtered(lambda l: l.state == 'sale'):
-            invoice_lines = line.invoice_lines.filtered(
-                lambda l: l.invoice_id.state in {'open', 'in_payment', 'paid'})
-            amount_invoiced = 0.0
-            for inv_line in invoice_lines:
-                inv_date = (inv_line.invoice_id.date_invoice
-                            or fields.Date.today())
-                amount = inv_line.currency_id._convert(
-                    inv_line.price_total, line.company_id.currency_id,
-                    line.company_id, inv_date)
-                if inv_line.invoice_id.type == 'out_invoice':
-                    amount_invoiced += amount
-                elif inv_line.invoice_id.type == 'out_refund':
-                    amount_invoiced -= amount
+                 'product_uom_qty',
+                 'qty_invoiced')
+    def _compute_risk_amount(self):
+        for line in self:
+            if line.state != 'sale':
+                line.risk_amount = 0.0
+                continue
+            qty = line.product_uom_qty
             if line.product_id.invoice_policy == 'delivery':
-                total_sale_line = line.price_reduce_taxinc * line.qty_delivered
+                qty = max(qty, line.qty_delivered)
+            risk_qty = float_round(
+                qty - line.qty_invoiced,
+                precision_rounding=line.product_uom.rounding)
+            # There is no risk if the line hasn't stock moves to deliver
+            # Added hasattr condition because fails in post-migration compute
+            if risk_qty and line.qty_delivered_method == 'stock_move' and (
+                    hasattr(line, 'move_ids')):
+                if not line.move_ids.filtered(
+                        lambda move: move.state not in ('done', 'cancel')):
+                    risk_qty = line.qty_to_invoice
+            if risk_qty == 0.0:
+                line.risk_amount = 0.0
+                continue
+            if line.product_uom_qty:
+                # This method has more precision that using price_reduce_taxinc
+                risk_amount = line.price_total * (
+                    risk_qty / line.product_uom_qty)
             else:
-                total_sale_line = line.price_total
-            total_sale_line = line.currency_id._convert(
-                total_sale_line, line.company_id.currency_id,
+                risk_amount = line.price_reduce_taxinc * risk_qty
+            line.risk_amount = line.order_id.currency_id._convert(
+                risk_amount, line.company_id.currency_id,
                 line.company_id, fields.Date.today())
-            line.amt_to_invoice = total_sale_line - amount_invoiced
