2d060fe364016da610994754aa2ee310454c5fbd
mreficent <miquel.raich@eficent.com>
2020-06-08 14:20:10 +0200

    [IMP] account_financial_risk: black, isort, prettier

--- account_financial_risk/__manifest__.py
+++ account_financial_risk/__manifest__.py
@@ -2,23 +2,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Financial Risk',
-    'summary': 'Manage customer risk',
-    'version': '12.0.2.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'author': 'Tecnativa, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/credit-control',
-    'depends': [
-        'account',
+    "name": "Account Financial Risk",
+    "summary": "Manage customer risk",
+    "version": "12.0.2.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/credit-control",
+    "depends": ["account"],
+    "data": [
+        "views/account_financial_risk_view.xml",
+        "views/res_config_view.xml",
+        "views/res_partner_view.xml",
+        "wizards/account_invoice_state_view.xml",
+        "wizards/partner_risk_exceeded_view.xml",
+        "templates/assets.xml",
     ],
-    'data': [
-        'views/account_financial_risk_view.xml',
-        'views/res_config_view.xml',
-        'views/res_partner_view.xml',
-        'wizards/account_invoice_state_view.xml',
-        'wizards/partner_risk_exceeded_view.xml',
-        'templates/assets.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
