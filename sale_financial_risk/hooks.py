61e8a6334bbf255322dae7aaf47c34e49f101dbf
mreficent <miquel.raich@eficent.com>
2020-06-15 10:57:34 +0200

    [IMP] sale_financial_risk: black, isort, prettier

--- sale_financial_risk/hooks.py
+++ sale_financial_risk/hooks.py
@@ -1,9 +1,10 @@
 # Copyright 2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo.tools import sql
 
-import logging
 logger = logging.getLogger(__name__)
 
 
@@ -16,11 +17,10 @@ def pre_init_hook(cr):
 
 
 def create_commercial_partner_id_column(cr):
-    if not sql.column_exists(cr, 'sale_order_line', 'commercial_partner_id'):
-        sql.create_column(
-            cr, 'sale_order_line', 'commercial_partner_id', 'int4')
+    if not sql.column_exists(cr, "sale_order_line", "commercial_partner_id"):
+        sql.create_column(cr, "sale_order_line", "commercial_partner_id", "int4")
 
-    logger.info('Computing field commercial_partner_id on sale.order.line')
+    logger.info("Computing field commercial_partner_id on sale.order.line")
     cr.execute(
         """
         UPDATE sale_order_line sol
