ed493f1c9df213acfd31e8fabb43af67fdd205b1
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-15 18:38:14 +0200

    Add module account_invoice_overdue_reminder

---
+++ account_invoice_overdue_reminder/migrations/12.0.2.0.0/pre-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    cr.execute(
+        'DELETE from overdue_reminder_action')
