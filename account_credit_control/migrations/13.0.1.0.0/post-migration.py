a9ef26048e21b777685db3804918bd0bb1b32621
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-20 20:04:52 +0200

    [OU] account_credit_control: OpenUpgrade migration scripts

--- account_credit_control/migrations/13.0.1.0.0/post-migration.py
+++ account_credit_control/migrations/13.0.1.0.0/post-migration.py
@@ -9,3 +9,10 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "account_credit_control", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
+    openupgrade.logged_query(
+        env.cr,
+        """UPDATE account_move am
+        SET credit_policy_id = ai.credit_policy_id
+        FROM account_invoice ai
+        WHERE ai.move_id = am.id""",
+    )
---
+++ account_credit_control/migrations/13.0.1.0.0/pre-migration.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+from psycopg2 import sql
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.copy_columns(
+        env.cr, {"credit_control_line": [("invoice_id", None, None)]}
+    )
+    openupgrade.logged_query(
+        env.cr,
+        sql.SQL(
+            """UPDATE credit_control_line ccl
+            SET invoice_id = ai.move_id
+            FROM account_invoice ai
+            WHERE ai.id = ccl.{}"""
+        ).format(sql.Identifier(openupgrade.get_legacy_name("invoice_id"))),
+    )
