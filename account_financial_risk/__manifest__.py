f9b564684cdb0ee7f335c06d6570c5c7a0a6af20
AaronHForgeFlow <ahenriquez@eficent.com>
2020-10-21 11:08:50 +0200

    [IMP] account_financial_risk: Add specific overpassing group
    This commit adds an specific group for overpassing partner credit limit exceptions
    In some situations, not only account managers shoud be allowed to overpass exception, depending on the company policies. Merging this code will not affect current instances, as long as account managers will be added to the new group.

--- account_financial_risk/__manifest__.py
+++ account_financial_risk/__manifest__.py
@@ -4,13 +4,14 @@
 {
     "name": "Account Financial Risk",
     "summary": "Manage customer risk",
-    "version": "13.0.2.0.0",
+    "version": "13.0.2.1.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/credit-control",
     "depends": ["account"],
     "data": [
+        "security/security.xml",
         "views/account_financial_risk_view.xml",
         "views/res_config_view.xml",
         "views/res_partner_view.xml",
