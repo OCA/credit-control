6c4a97047ac8b46ce8a038dd3d4b33c31a3070b7
Ugne Sinkeviciene <ugne.piscikaite@gmail.com>
2020-12-07 21:17:10 +0200

    [MIG] stock_financial_risk: Migration to 14.0

--- stock_financial_risk/__manifest__.py
+++ stock_financial_risk/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Partner Stock Risk",
     "summary": "Manage partner risk in stock moves",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
