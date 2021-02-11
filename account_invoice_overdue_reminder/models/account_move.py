f7420c84d3e1451d9d2e39b7202edb8af80b681e
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-11 19:00:25 +0100

    [MIG] account_invoice_overdue_reminder to v14.0

--- account_invoice_overdue_reminder/models/account_move.py
+++ account_invoice_overdue_reminder/models/account_move.py
@@ -1,16 +1,16 @@
-# Copyright 2020 Akretion France (http://www.akretion.com/)
+# Copyright 2020-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
     no_overdue_reminder = fields.Boolean(
         string='Disable Overdue Reminder',
-        track_visibility='onchange')
+        tracking=True)
     overdue_reminder_ids = fields.One2many(
         'account.invoice.overdue.reminder',
         'invoice_id',
@@ -29,17 +29,18 @@ class AccountInvoice(models.Model):
         'CHECK(overdue_reminder_counter >= 0)',
         'Overdue Invoice Counter must always be positive')]
 
-    @api.depends('type', 'state', 'date_due')
+    @api.depends('move_type', 'state', 'payment_state', 'invoice_date_due')
     def _compute_overdue(self):
         today = fields.Date.context_today(self)
-        for inv in self:
+        for move in self:
             overdue = False
             if (
-                    inv.type == 'out_invoice' and
-                    inv.state == 'open' and
-                    inv.date_due < today):
+                    move.move_type == 'out_invoice' and
+                    move.state == 'posted' and
+                    move.payment_state not in ('paid', 'reversed', 'in_payment') and
+                    move.invoice_date_due < today):
                 overdue = True
-            inv.overdue = overdue
+            move.overdue = overdue
 
     @api.depends(
         'overdue_reminder_ids.action_id.date',
@@ -47,14 +48,14 @@ class AccountInvoice(models.Model):
         'overdue_reminder_ids.action_id.reminder_type')
     def _compute_overdue_reminder(self):
         aioro = self.env['account.invoice.overdue.reminder']
-        for inv in self:
+        for move in self:
             reminder = aioro.search(
-                [('invoice_id', '=', inv.id)], order='action_date desc', limit=1)
+                [('invoice_id', '=', move.id)], order='action_date desc', limit=1)
             date = reminder and reminder.action_date or False
             counter_reminder = aioro.search([
-                ('invoice_id', '=', inv.id),
+                ('invoice_id', '=', move.id),
                 ('action_reminder_type', 'in', ('mail', 'post'))],
                 order='action_date desc, id desc', limit=1)
             counter = counter_reminder and counter_reminder.counter or False
-            inv.overdue_reminder_last_date = date
-            inv.overdue_reminder_counter = counter
+            move.overdue_reminder_last_date = date
+            move.overdue_reminder_counter = counter
--- account_invoice_overdue_reminder/readme/USAGE.rst
+++ account_invoice_overdue_reminder/readme/USAGE.rst
@@ -1,6 +1,6 @@
 Of course, before sending invoice reminders, you must import your bank statements and process them, so that you are up-to-date on customer payments.
 
-Then, go to the menu *Invoicing > Accounting > Actions > Overdue Invoice Remind*: you will get the start screen where you can:
+Then, go to the menu *Invoicing > Customers > Overdue Invoice Remind*: you will get the start screen where you can:
 
 * filter the customers that you want to remind (filter by customer or by salesman),
 * check that your bank journals are up-to-date,
