0e2d54031aa782ab92d22a42fb00fe53bec274d4
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-11 19:04:15 +0100

    account_invoice_overdue_reminder: black, isort and other reformatting

--- account_invoice_overdue_reminder/models/overdue_reminder_result.py
+++ account_invoice_overdue_reminder/models/overdue_reminder_result.py
@@ -6,15 +6,14 @@ from odoo import fields, models
 
 
 class OverdueReminderResult(models.Model):
-    _name = 'overdue.reminder.result'
-    _description = 'Overdue Invoice Reminder Result/Info'
-    _order = 'sequence, id desc'
+    _name = "overdue.reminder.result"
+    _description = "Overdue Invoice Reminder Result/Info"
+    _order = "sequence, id desc"
 
     name = fields.Char(required=True, translate=True)
     active = fields.Boolean(default=True)
     sequence = fields.Integer()
 
-    _sql_constraints = [(
-        'name_unique',
-        'unique(name)',
-        'This overdue reminder result already exists')]
+    _sql_constraints = [
+        ("name_unique", "unique(name)", "This overdue reminder result already exists")
+    ]
