72d06292488cf18f7c6309275b226aca469b6dd9
Carlos Dauden <carlos.dauden@tecnativa.com>
2021-09-03 10:16:42 +0200

    [FIX] account_financial_risk: Show ValidationError when post multiple invoices from wizard. Avoid singleton with multi-company _autopost_draft_entries.  When we post from validate.account.move wizard, validate_move returns 'ir.actions.act_window_close'.  Fixed issue #147  TT30969

--- account_financial_risk/models/account_invoice.py
+++ account_financial_risk/models/account_invoice.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AccountMove(models.Model):
@@ -68,18 +69,30 @@ class AccountMove(models.Model):
         for invoice in self.filtered(lambda x: x.move_type == "out_invoice"):
             exception_msg = invoice.risk_exception_msg()
             if exception_msg:
-                return (
-                    self.env["partner.risk.exceeded.wiz"]
-                    .create(
-                        {
-                            "exception_msg": exception_msg,
-                            "partner_id": invoice.partner_id.commercial_partner_id.id,
-                            "origin_reference": "{},{}".format(
-                                "account.move", invoice.id
-                            ),
-                            "continue_method": "action_post",
-                        }
+                # When we post from validate.account.move wizard, validate_move returns
+                # 'ir.actions.act_window_close'
+                # Check active model to raise exception instead of returns wizard
+                if self.env.context.get("active_model") == "account.move":
+                    return (
+                        self.env["partner.risk.exceeded.wiz"]
+                        .create(
+                            {
+                                "exception_msg": exception_msg,
+                                "partner_id": invoice.partner_id.commercial_partner_id.id,
+                                "origin_reference": "{},{}".format(
+                                    "account.move", invoice.id
+                                ),
+                                "continue_method": "post",
+                            }
+                        )
+                        .action_show()
                     )
-                    .action_show()
-                )
+                else:
+                    raise ValidationError(
+                        _(
+                            "The partner %s is in risk exception.\n"
+                            "You must post his invoices from form view to allow over risk"
+                        )
+                        % invoice.partner_id.commercial_partner_id.display_name
+                    )
         return super().action_post()
