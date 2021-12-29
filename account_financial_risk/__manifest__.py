1509ec45a92dd8c011255a676987fe584c9d6264
Carlos Dauden <carlos.dauden@tecnativa.com>
2021-12-29 09:42:03 +0100

    [FIX] account_financial_risk: Remove customer_rank condition to show financial risk info in partner
    TT33665

--- account_financial_risk/__manifest__.py
+++ account_financial_risk/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Financial Risk",
     "summary": "Manage customer risk",
-    "version": "15.0.1.0.2",
+    "version": "15.0.1.1.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
