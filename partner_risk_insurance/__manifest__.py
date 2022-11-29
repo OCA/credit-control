af15efc798f4de2124c5f32b46e39d386bb6597f
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-29 08:35:47 +0100

    [IMP] partner_risk_insurance: Change SavepointCase to TransactionCase
    partner_risk_insurance 15.0.1.0.1

--- partner_risk_insurance/__manifest__.py
+++ partner_risk_insurance/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Risk Insurance",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "development_status": "Production/Stable",
     "summary": "Risk insurance partner information",
     "author": "AvanzOSC,"
