16e7a2521c8c05e2812c1e0e0b52dd8c6ce06648
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-11-30 17:11:20 +0100

    [MIG] sale_financial_risk_info: Migration to 15.0

--- sale_financial_risk_info/__manifest__.py
+++ sale_financial_risk_info/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Financial Risk Info",
     "summary": "Adds risk consumption info in sales orders.",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
