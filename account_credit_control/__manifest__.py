ac00404b09e7e07f490cf2ffab54f4b7329a3a18
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-13 12:34:50 +0100

    [IMP] account_credit_control: Add domain to email_template_id field.

--- account_credit_control/__manifest__.py
+++ account_credit_control/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Credit Control",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.1.1",
     "author": "Camptocamp,"
     "Odoo Community Association (OCA),"
     "Okia,"
