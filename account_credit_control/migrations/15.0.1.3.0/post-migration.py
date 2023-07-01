ee03a91ae7ee1d1fb4dd69b0eb37314bcca003af
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-07-01 18:46:27 +0200

    [OU-FIX] account_credit_control: Delete noupdate record translations

--- account_credit_control/migrations/15.0.1.3.0/post-migration.py
+++ account_credit_control/migrations/15.0.1.3.0/post-migration.py
@@ -9,3 +9,6 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "account_credit_control", "migrations/15.0.1.3.0/noupdate_changes.xml"
     )
+    openupgrade.delete_record_translations(
+        env.cr, "account_credit_control", ["email_template_credit_control_base"]
+    )
