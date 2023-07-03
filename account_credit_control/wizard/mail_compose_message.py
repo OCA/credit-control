1b4f68c87b5ede2f63efdb48e535c1f691876044
Víctor Martínez <victor.martinez@tecnativa.com>
2023-07-03 08:18:19 +0200

    [FIX] account_credit_control: Add table to email content

--- account_credit_control/wizard/mail_compose_message.py
+++ account_credit_control/wizard/mail_compose_message.py
@@ -1,14 +1,16 @@
 # Copyright 2023 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from markupsafe import Markup
+
 from odoo import models
 
 
 class MailComposer(models.TransientModel):
     _inherit = "mail.compose.message"
 
-    def onchange_template_id(self, template_id, composition_mode, model, res_id):
-        res = super().onchange_template_id(
+    def _onchange_template_id(self, template_id, composition_mode, model, res_id):
+        res = super()._onchange_template_id(
             template_id=template_id,
             composition_mode=composition_mode,
             model=model,
@@ -16,5 +18,7 @@ class MailComposer(models.TransientModel):
         )
         if self.env.context.get("inject_credit_control_communication_table"):
             record = self.env[model].browse(res_id)
-            res["value"]["body"] += record._get_credit_control_communication_table()
+            res["value"]["body"] += Markup(
+                record._get_credit_control_communication_table()
+            )
         return res
