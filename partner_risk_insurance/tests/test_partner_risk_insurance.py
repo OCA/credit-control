247bf86f237938e3d7140bcdd00fbad458b5ffe2
Víctor Martínez <victor.martinez@tecnativa.com>
2023-08-16 13:49:35 +0200

    [MIG] partner_risk_insurance: Migration to 16.0
    TT44335

--- partner_risk_insurance/tests/test_partner_risk_insurance.py
+++ partner_risk_insurance/tests/test_partner_risk_insurance.py
@@ -1,6 +1,9 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
+# Copyright 2023 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import TransactionCase, tagged
+from odoo.tests import Form, TransactionCase, tagged
+
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
 
 
 @tagged("post_install", "-at_install")
@@ -8,16 +11,7 @@ class TestSaleOrderLineInput(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(
-            context=dict(
-                cls.env.context,
-                mail_create_nolog=True,
-                mail_create_nosubscribe=True,
-                mail_notrack=True,
-                no_reset_password=True,
-                tracking_disable=True,
-            )
-        )
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.partner = cls.env["res.partner"].create(
             {
                 "name": "Test",
@@ -28,3 +22,11 @@ class TestSaleOrderLineInput(TransactionCase):
 
     def test_credit_limit(self):
         self.assertEqual(self.partner.credit_limit, 150.00)
+        # set global credit_limit
+        form = Form(self.env["res.config.settings"].sudo())
+        form.account_use_credit_limit = True
+        form.account_default_credit_limit = 100
+        form.save()
+        # update company_credit_limit
+        self.partner.company_credit_limit = 200
+        self.assertEqual(self.partner.credit_limit, 350.00)  # 350 = 100 + 200 + 50
