2b5d7b406fb14c030dac1e947c1578d0580978f9
Eduardo De Miguel <edu@moduon.team>
2023-09-28 12:12:45 +0200

    [FIX] sale_financial_risk: Make stored computed risk fields

--- sale_financial_risk/models/res_partner.py
+++ sale_financial_risk/models/res_partner.py
@@ -17,6 +17,8 @@ class ResPartner(models.Model):
     )
     risk_sale_order = fields.Monetary(
         compute="_compute_risk_sale_order",
+        compute_sudo=True,
+        store=True,
         string="Total Sales Orders Not Invoiced",
         currency_field="risk_currency_id",
         help="Total not invoiced of sales orders in Sale Order state",
