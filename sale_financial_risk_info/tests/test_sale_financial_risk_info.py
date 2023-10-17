70bd98a634a780d5c50ac9f5dfab7eb11371058a
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-17 15:38:56 +0200

    [IMP] sale_financial_risk_info: test performance improvement - Switch to setUpClass (if needed) for avoiding repeat the same setup for each test. - Include context keys for avoiding mail operations overhead.

--- sale_financial_risk_info/tests/test_sale_financial_risk_info.py
+++ sale_financial_risk_info/tests/test_sale_financial_risk_info.py
@@ -5,44 +5,56 @@ from odoo.tests.common import Form, TransactionCase
 
 
 class TestSaleFinancialRiskInfo(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.Partner = self.env["res.partner"]
-        self.Product = self.env["product.product"]
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
+        cls.Partner = cls.env["res.partner"]
+        cls.Product = cls.env["product.product"]
 
-        self.product_1 = self.Product.create(
+        cls.product_1 = cls.Product.create(
             {"name": "Product test 1", "list_price": 1000.00}
         )
-        self.partner_1 = self.Partner.create(
+        cls.partner_1 = cls.Partner.create(
             {
                 "name": "partner test rebate 1",
                 "ref": "TST-001",
                 "risk_invoice_draft_include": True,
             }
         )
-        self.default_account_revenue = self.env["account.account"].search(
+        cls.default_account_revenue = cls.env["account.account"].search(
             [
-                ("company_id", "=", self.env.company.id),
+                ("company_id", "=", cls.env.company.id),
                 (
                     "user_type_id",
                     "=",
-                    self.env.ref("account.data_account_type_revenue").id,
+                    cls.env.ref("account.data_account_type_revenue").id,
                 ),
             ],
             limit=1,
         )
-        self.invoice_partner_1 = self.create_invoice(self.partner_1)
+        cls.invoice_partner_1 = cls.create_invoice(cls.partner_1)
 
     # Create some invoices for partner
-    def create_invoice(self, partner):
+    @classmethod
+    def create_invoice(cls, partner):
         move_form = Form(
-            self.env["account.move"].with_context(default_type="out_invoice")
+            cls.env["account.move"].with_context(default_type="out_invoice")
         )
         move_form.ref = "Test Customer Invoice"
         move_form.partner_id = partner
         with move_form.invoice_line_ids.new() as line_form:
-            line_form.product_id = self.product_1
-            line_form.account_id = self.default_account_revenue
+            line_form.product_id = cls.product_1
+            line_form.account_id = cls.default_account_revenue
         return move_form.save()
 
     def test_sale_order_risk_info(self):
