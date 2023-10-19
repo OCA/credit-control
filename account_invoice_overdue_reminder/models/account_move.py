caf93f298c701f7937fbd6cbbc6621e1c697f9bc
Andrea Stirpe <a.stirpe@onestein.nl>
2023-10-19 09:39:55 +0200

    [16.0] account_invoice_overdue_reminder: remind partially paid invoices

--- account_invoice_overdue_reminder/models/account_move.py
+++ account_invoice_overdue_reminder/models/account_move.py
@@ -45,7 +45,7 @@ class AccountMove(models.Model):
             if (
                 move.move_type == "out_invoice"
                 and move.state == "posted"
-                and move.payment_state == "not_paid"
+                and move.payment_state in ("not_paid", "partial")
                 and move.invoice_date_due
                 and move.invoice_date_due < today
             ):
