f00ddfd1de5846aff5483cc2bf3f29d920573964
Quoc - Pham Ngoc <quoc-pn@komit-consulting.com>
2023-11-30 10:37:23 +0700

    [MIG] partner_credit_limit_history: Migration to 14.0

--- partner_credit_limit_history/__manifest__.py
+++ partner_credit_limit_history/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Credit Limit History",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Credit Control",
     "website": "https://github.com/OCA/credit-control",
     "author": "Tecnativa, Odoo Community Association (OCA)",
