5ce455a170cfc855ce3b3a92dde1ca814fa9dc51
José Moreno Hanshing <jose@blancomartin.cl>
2024-06-12 12:08:33 -0400

    [MIG]sale_financial_risk: studio73 migration el PR está aprobado pero inactivo hace 2 semanas

---
+++ sale_financial_risk/models/payment.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class PaymentTransaction(models.Model):
+    _inherit = "payment.transaction"
+
+    def _set_authorized(self):
+        """Bypass risk for sale confirmation triggered by this method"""
+        return super(
+            PaymentTransaction, self.with_context(bypass_risk=True)
+        )._set_authorized()
+
+    def _reconcile_after_done(self):
+        """Bypass risk for sale confirmation and invoice creation triggered
+        by this method
+        """
+        return super(
+            PaymentTransaction, self.with_context(bypass_risk=True)
+        )._reconcile_after_done()
