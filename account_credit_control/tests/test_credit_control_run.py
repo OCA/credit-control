860a6854b2dd4e36f240cbcb5bdb1b00a187f352
len <len@lambdao.dev>
2025-07-09 23:48:44 +0200

    [FIX] account_credit_control: more flexible tests
    The root tag for the message could be a span in certain cases,
    so these are excluded from the check.
    
    The email_from is defined in the template, so it should be set on the
    company to be able to send it.

--- account_credit_control/tests/test_credit_control_run.py
+++ account_credit_control/tests/test_credit_control_run.py
@@ -130,10 +130,10 @@ class TestCreditControlRun(AccountTestInvoicingCommon):
         )
 
         report_regex = (
-            r'<p>Policy "<b>%s</b>" has generated <b>'
-            r"\d+ Credit Control Lines.</b><br></p>" % self.policy.name
+            r'Policy "<b>%s</b>" has generated <b>'
+            r"\d+ Credit Control Lines.</b><br>" % self.policy.name
         )
-        regex_result = re.match(report_regex, control_run.report)
+        regex_result = re.search(report_regex, control_run.report)
         self.assertIsNotNone(regex_result)
 
     def test_generate_credit_lines_with_max_level(self):
@@ -216,10 +216,10 @@ class TestCreditControlRun(AccountTestInvoicingCommon):
         self.assertEqual(control_run.state, "done")
 
         report_regex = (
-            r'<p>Policy "<b>%s</b>" has generated <b>'
-            r"\d+ Credit Control Lines.</b><br></p>" % self.policy.name
+            rf'Policy "<b>{self.policy.name}</b>" has generated <b>'
+            r"\d+ Credit Control Lines.</b><br>"
         )
-        regex_result = re.match(report_regex, control_run.report)
+        regex_result = re.search(report_regex, control_run.report)
         self.assertIsNotNone(regex_result)
 
         # Mark lines to be send
@@ -251,8 +251,8 @@ class TestCreditControlRun(AccountTestInvoicingCommon):
         self.assertEqual(control_run.state, "done")
 
         report_regex = (
-            r'<p>Policy "<b>%s</b>" has generated <b>'
-            r"\d+ Credit Control Lines.</b><br></p>" % self.policy.name
+            rf'<p>Policy "<b>{self.policy.name}</b>" has generated <b>'
+            r"\d+ Credit Control Lines.</b><br></p>"
         )
         regex_result = re.match(report_regex, control_run.report)
         self.assertIsNotNone(regex_result)
@@ -294,6 +294,7 @@ class TestCreditControlRun(AccountTestInvoicingCommon):
         emailer_obj = self.env["credit.control.emailer"]
         wiz_emailer = emailer_obj.create({})
         wiz_emailer.line_ids = control_lines
+        self.env.user.company_id.email = "test@example.com"
         with RecordCapturer(self.env["credit.control.communication"], []) as capture:
             wiz_emailer.email_lines()
         new_communication = capture.records
