7a60a51c9082fc3a48956b2e24242bff1662b526
Eduardo de Miguel <edu@moduon.team>
2025-07-16 09:45:26 +0200

    [MIG] partner_risk_insurance_product_sticker_invoice_report: Migration to 18.0

--- partner_risk_insurance_product_sticker_invoice_report/__manifest__.py
+++ partner_risk_insurance_product_sticker_invoice_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Sticker on Invoice Reports with Risk Insurance",
     "summary": "Display a Sticker on Invoice Reports secured with Risk Insurance",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/credit-control",
