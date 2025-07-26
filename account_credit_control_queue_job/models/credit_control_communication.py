920d297f9538e276ad7c5976ef90ce0d4e891f2f
len <len@lambdao.dev>
2025-07-26 11:30:10 +0200

    [IMP] account_credit_control_queue_job: put jobs in batch

--- account_credit_control_queue_job/models/credit_control_communication.py
+++ account_credit_control_queue_job/models/credit_control_communication.py
@@ -15,9 +15,13 @@ class CreditControlCommunication(models.Model):
             batch_size = max(1, int(batch_size))
         except Exception:  # pylint: disable=broad-except
             batch_size = 1
+        batch_name = _("Credit Control Emails")
+        batch = self.env["queue.job.batch"].get_new_batch(batch_name)
         for comms in split_every(batch_size, self.ids, self.browse):
             if batch_size > 1:
                 desc = _("Sending credit control emails for ids: %s") % comms.ids
             else:
                 desc = _("Sending credit control email for %s") % comms.partner_id.name
-            comms.with_delay(description=desc)._send_communications_by_email()
+            comms.with_context(job_batch=batch).with_delay(
+                description=desc
+            )._send_communications_by_email()
