eb330bf3253fdd6c9bbac1c5e1bc3cd87ef597af
Quentin Groulard <quentin.groulard@acsone.eu>
2025-11-03 16:53:02 +0100

    [IMP] account_credit_control: multi-channel policies migration script

---
+++ account_credit_control/migrations/18.0.3.0.0/pre-migration.py
@@ -0,0 +1,28 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    # Preserve channel historic data from credit.control.line records
+    if openupgrade.column_exists(env.cr, "credit_control_line", "channel"):
+        openupgrade.copy_columns(
+            env.cr,
+            {
+                "credit_control_line": [
+                    ("channel", "channel_old", None),
+                ],
+            },
+        )
+    if openupgrade.column_exists(env.cr, "credit_control_policy_level", "channel"):
+        openupgrade.copy_columns(
+            env.cr,
+            {
+                "credit_control_policy_level": [
+                    ("channel", "channel_old", None),
+                ],
+            },
+        )
