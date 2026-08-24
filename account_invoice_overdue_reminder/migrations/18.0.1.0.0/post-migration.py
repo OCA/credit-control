d1c24e8be999722d1ff62d86da84de277230ef31
LorenzoC0 <lorenzo.carta@innovyou.it>
2026-08-24 11:31:34 +0200

    [FIX] account_invoice_overdue_reminder: skip company-dependent conversion when ir_property table is already gone

--- account_invoice_overdue_reminder/migrations/18.0.1.0.0/post-migration.py
+++ account_invoice_overdue_reminder/migrations/18.0.1.0.0/post-migration.py
@@ -5,4 +5,8 @@ from openupgradelib import openupgrade, openupgrade_180
 
 @openupgrade.migrate()
 def migrate(env, version):
+    if not openupgrade.table_exists(env.cr, "ir_property"):
+        # The database may have been already touched by Odoo's
+        # official migration scripts
+        return
     openupgrade_180.convert_company_dependent(env, "res.partner", "no_overdue_reminder")
